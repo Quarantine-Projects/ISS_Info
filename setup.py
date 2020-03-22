4eb11d665a66ffef9ea573b18f67094a62f50e32
Salil Gautam <salilgautam_15037@aitpune.edu.in>
2020-03-22 16:48:39 +0530

    Updating supported python versions

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
@@ -29,5 +28,5 @@ setup(
     'Programming Language :: Python :: 3.8',
     'Operating System :: OS Independent',
   ],
-  python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*'
+  python_requires='>=3, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*'
 )
