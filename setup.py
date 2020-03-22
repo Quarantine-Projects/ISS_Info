2a869bacedb5d1a8a2eb9d2c067ee9727ce6a718
salil-gtm <salilgautam_15037@aitpune.edu.in>
2020-03-22 20:12:25 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import find_packages, setup
 
 # read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
   name = 'ISS_Info',
   packages = find_packages(include=["ISS_Info"]),
-  version = '1.0',
+  version = '1.0.0',
   license='MIT',
   description = 'Python wrapper for tracking information about International Space Station via http://open-notify.org/',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Salil Gautam & Prakhar Dixit',
   url = 'https://github.com/Quarantine-Projects/ISS_Info',
   keywords = ['ISS', 'Space', 'Station','API'],
   install_requires=["requests>=2.23.0"],
   classifiers=[
-    'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
